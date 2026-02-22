fccac3955f84364cf03a69e32475453e86025f29
zoikle <134870626+Gros26@users.noreply.github.com>
2026-02-22 14:16:22 -0500

    feat: add Sidebar, MainLayout and nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './styles/App.css'
-import api from './services/api'
+import MainLayout from './components/layout/MainLayout'
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/*Redirige la raiz a /hoy */}
-        <Route path="/" element={<Navigate to="/hoy" replace/>}/>
         <Route path="/login" element={<div>Login - Próximamente</div>} />
-        <Route path="/hoy" element={<div>Hoy - Próximamente</div>} />
-        <Route path="/crear" element={<div>Crear - Próximamente</div>} />
-        <Route path="/actividad/:id" element={<div>Detalle - Próximamente</div>} />
-        <Route path="/progreso" element={<div>Progreso - Próximamente</div>} />
+        {/*Como todas estan rutas comparten el Sidebar las agrupamos aqui*/}
+        <Route element={<MainLayout />}>
+          {/*Redirige la raiz a /hoy */}
+          <Route path="/" element={<Navigate to="/hoy" replace/>}/>
+          <Route path="/hoy" element={<div>Hoy - Próximamente</div>} />
+          <Route path="/crear" element={<div>Crear - Próximamente</div>} />
+          <Route path="/actividad/:id" element={<div>Detalle - Próximamente</div>} />
+          <Route path="/progreso" element={<div>Progreso - Próximamente</div>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
